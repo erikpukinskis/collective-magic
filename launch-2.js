8e5b554a2186a254dbdfd7df5bd66125e3d86aef
Erik Pukinskis <erik@snowedin.net>
2018-03-10 13:30:58 -0800

    Document the work process that's been happening with Bobby

---
+++ launch-2.js
@@ -0,0 +1,156 @@
+// Ezjs
+// Geography
+// Family lineage
+// A wild universe appeared
+// Bond
+// Tasks
+// Expenses
+// Buy
+// Tiny house
+
+function ( bond ) {
+  bond (
+    "workshop" ) }
+
+function manager (   ) {
+  everyDay (
+    function (   ) {
+      var bobby = "XXX-XXX-XXXX"
+      var foreman = bobby
+      call (
+        foreman ,
+        makeBudget ,
+        payOut )
+
+function makeBudget ( foreman, done ) {
+  ask (
+    foreman ,
+    "What are the next steps to do today" ,
+    ask.for.lines ,
+    addTasks , 
+    askForHours ) }
+
+function addTasks ( tasks, done ) {
+  issueBond.tasks (
+    bondId ,
+    tasks )
+  done ( ) }
+
+function askForHours ( done ) {
+  ask (
+    foreman ,
+    "How many hours will each of these tasks take?",
+    tasks.map (
+      ask.for.hours ) ,
+    addLaborExpense ,
+    askForMaterials }
+
+
+
+  function askForMaterials ( estimates ) {
+    issueBond.expense (
+      "labor on "+ dateString(),
+      sum (
+        estimates ) )
+
+    ask (
+      foreman ,
+      "What materials do you need?",
+      ask.for.lines ,
+       )
+
+  var prices = ask (
+    foreman ,
+    "Who much are each of these items?" ,
+    materials.map (
+      ask.for.price ) )
+
+  var name = "Day of work: " 
+
+  var workdayId = addDashes(name)
+
+  var SQUAD_HOURLY_AVERAGE = 1500
+
+  ask.for (
+    tasks ,
+    hours ,
+    materials ,
+    prices ,
+    function ( tasks, hours, materials, prices ) {
+
+      var labor = hours * SQUAD_HOURLY_AVERAGE
+
+      var totalPrices = sum (
+        prices )
+
+      advance = costs
+
+      issueBond.invoice (
+        workdayId ,
+        bondId ,
+        "Materials, advance on labor 2018-01-03",
+        totalPrices + labor / 2,
+        "2018-01-03" )
+
+      done (   ) ) }
+
+
+
+function tabulateInvoice ( lines, estimates )
+
+
+    "How many long will it take to do "+task+" for two people? (hours)",
+    function ( hours ) {
+      bond.expense(hours+ " hours labor, @$30/hr to be split between two people", 30*hours*100 ) },
+    done ) }
+
+
+    bond.expense (
+      "workshop" ,
+      "build roof, 8 hours" ,
+      12 * 1500 )
+
+    bond.registerInvestor(
+      "erik",
+      "Erik Pukinskis",
+      "XXX-XXX-XXXX")
+
+    bond.orderShares(
+      "eriks-shares" ,
+      "workshop" ,    
+      "erik" ,        
+      22500 ,         
+      16875 )         
+
+    bond.invoice (
+      "workshop" ,
+      "jan-7-2018@bobby" 
+    )
+
+    bond.lineItem (
+      "workshop" ,
+      "jan-7-2108@bobby" ,
+      "build roof, 6 hours" ,
+      6 * 1500 )
+
+    bond.settleUp (
+      "workshop" ,
+      "jan-7-2018@bobby",
+      12000,
+      500 )
+
+    bond.revenue (
+      "workshop" ,
+      "dividends from 36225 shares of tiny-house-1-sale" ,
+      36225 )
+
+    bond.mature (
+      "workshop",
+      "eriks-shares",
+      "eriks-buyout",
+      32850 )
+
+
+// Video moment (mux)
+// Answer emails
+// getCustomer
