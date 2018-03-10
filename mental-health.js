59dde1123b24a72c825dcf708940a9c59034d111
Erik Pukinskis <erik@snowedin.net>
2018-03-10 13:25:47 -0800

    mental-health.js

---
+++ mental-health.js
@@ -0,0 +1,15 @@
+function mentalHealth(){
+  return [
+    "creative outlet",
+    "exercise",
+    "routine",
+    "sleep",
+    "meditation",
+    "social activity",
+    "healthy diet",
+    "love",
+    "sun",
+    "air",
+    "humor",
+    "therapy"]}
+// Source: http://www.iamhoneydill.com/new-stuff/2018/3/2/the-mental-health-all-stars
